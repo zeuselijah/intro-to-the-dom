e6a674b787168003ada3919771cd0b8bc2dfbaaa
zeuselijah <simeonhendricks@gmail.com>
2022-08-05 13:48:52 -0400

    wip: adds and select p element

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // alert('JavaScript is linked!');
-const h1Element = document.getElementById('title');
-console.log(h1Element);
-h1Element.style.color = 'red';
 
+//select an element using getElement
+// const h1Element = document.getElementById('title');
+// console.log(h1Element);
+// h1Element.style.color = 'red';
+
+
+//select an element using querySelector
+// works like a css selector
+// const h1Element = document.querySelector('.main-title');
+// console.log(h1Element);
+
+const pEl = document.querySelector('.cool');
+console.log(pEl);
