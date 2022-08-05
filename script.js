303f05851986be95f71528cfa3c21e028ededbae
zeuselijah <simeonhendricks@gmail.com>
2022-08-05 14:54:10 -0400

    wip: adds attributes

--- script.js
+++ script.js
@@ -3,13 +3,45 @@
 //select an element using getElement
 // const h1Element = document.getElementById('title');
 // console.log(h1Element);
-// h1Element.style.color = 'red';
 
 
 //select an element using querySelector
 // works like a css selector
-// const h1Element = document.querySelector('.main-title');
+const h1Element = document.querySelector('.main-title');
 // console.log(h1Element);
 
 const pEl = document.querySelector('.cool');
-console.log(pEl);
+
+//how to change the content of a DOM element
+
+//innerHTML - used for setting text and html
+
+pEl.innerHTML = "Comments for <strong>today</strong>";
+
+
+//textContent - used for setting text only
+
+// pEl.textContent = "Comments for <strong>today</strong>";
+
+//changing css style of an element using JavaScript
+
+h1Element.style.color = 'red';
+h1Element.style.fontFamily = 'Helvetica';
+h1Element.style.textAlign = 'center';
+
+pEl.style.color = 'rebeccapurple';
+
+
+//working with HTML attributes and the DOM
+console.log(pEl.hasAttribute('class'));
+
+pEl.setAttribute('id', 'not-cool');
+
+console.log(h1Element.getAttribute('id'));
+
+const aEl = document.querySelector('a');
+aEl.setAttribute('href', 'https://google.com');
+aEl.setAttribute('target', '_blank');
+
+
+
